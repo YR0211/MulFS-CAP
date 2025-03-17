c477f679db8ef15b35206e46dcd8a3143b9da04e
Zengyi Yang <83146164+YR0211@users.noreply.github.com>
2025-03-17 15:58:27 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -16,10 +16,12 @@ def save_state_dir(network, save_model_dir):
         state_dict[key] = state_dict[key].to(torch.device('cpu'))
     torch.save(state_dict, save_model_dir)
 
-def load_state_dir(network, pretrain_dir, device):
-    network.load_state_dict({k.replace('module.', ''): v for k, v in torch.load(pretrain_dir).items()})
+
+def load_state_dir(network, ckpts, device):
+    network.load_state_dict({k.replace('module.', ''): v for k, v in ckpts.items()})
     network.to(device)
     network.eval()
+
 
 def save_img(x, save_dir):
     x = x.squeeze(dim=0).permute(1, 2, 0)
